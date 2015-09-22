8b50cadb8613ef696edfa54be94d39b7e4e30064
Jae Wook Lee <jwleemd@gmail.com>
2015-09-22 15:46:37 +0900

    CacheMatrix.R
    Jae Wook Lee's Week 2 Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,19 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+      x <<- y
+      m <<- NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(mat) m <<- mat
+  getInverse <- function() m
+  list(set = set, get = get, 
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
 }
 
 
@@ -12,4 +25,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {        # If there is already a cached matrix, print it
+        message("Getting the inverse for the input matrix...")
+        return(m)
+  }
+  
+  # If m is null, then determine the inverse for the input matrix
+  
+  data <- x$get()     # Reading in the original matrix
+  m <- solve(data)    # Calculating the inverse for the input matrix
+  x$setInverse(m)     # Sending the inverse to the cache
+  m                   # Returning the inverse
+  
 }
